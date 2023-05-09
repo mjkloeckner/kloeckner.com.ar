62aac7c4c0817b0909bcea942d76435ee718e54c
mjkloeckner <martinjkloeckner@gmail.com>
2023-05-09 15:23:55 -0300

    prompt before overwriting html folder

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
 root_folder="$1"
+html_folder="/var/www/html"
 
 if [ "$(id -u)" -ne "0" ] ; then
     echo "This script must be executed with root privileges."
     exit 1
 fi
 
+echo -n "override folder $html_folder with $root_folder? (Y/n)? "
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] || [ "$answer" = "" ]; then
+	rm -drf "$html_folder" >/dev/null 2>&1 ||:
+	cp -rf "$root_folder" "$html_folder"
+else
+	echo "skipping ..."
+fi
+
 set -xe
 
-rm -drf /var/www/html >/dev/null 2>&1 ||:
-
-cp -rf "$root_folder" /var/www/html
-
-rm /var/www/html/README.md &>/dev/null 2>&1 ||:
+rm "$html_folder"/README.md &>/dev/null 2>&1 ||:
 
 systemctl restart nginx
