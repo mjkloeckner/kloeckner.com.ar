52ef4fd773b7ff9ee0eee9043948da40ee8c9ebe
Martin Kloeckner <mjkloeckner@gmail.com>
2024-11-03 01:28:25 -0300

    fix total blog entries not counting properly

--- scripts/js/count-blog-posts.js
+++ scripts/js/count-blog-posts.js
@@ -2,10 +2,13 @@ var file;
 fetch('/common/blog_index.shtml')
 	.then(response => response.text())
 	.then((data) => {
+		let parser = new DOMParser();
+		let doc = parser.parseFromString(data, 'text/html');
+
 		// var match = data.match(/\r?\n/g);
 		// alert(match.length);
 		var content = document.getElementById('blog-entries');
-		var count = document.getElementById("blog-index-table").rows.length;
+		var count = doc.getElementById("blog-index-table").rows.length;
 		content.innerHTML += " (" + count + ")";
 	})
 
